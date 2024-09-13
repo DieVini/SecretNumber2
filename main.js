56e2cdf6838457395c574a6244431fdfb2d196b2
DieVini <diegovinicius.bh@gmail.com>
2024-09-13 12:18:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,69 @@
-varH1 = document.querySelector('h1');
-varH1.innerHTML = 'Secret Number';
-varP = document.querySelector('p');
-varP.innerHTML = 'Type a number from 1 to 10';
+//aumentar o intervalo de tentativas
+//limitar o numero de tentativas
+//desabilitar e habilitar o input
 
-let secret_number = Math.floor(Math.random()*10+1);
+//variaveis
+let secret_number = getSecretNumber();
 let trys = 1;
 
+//funções
+function getSecretNumber(){
+    return Math.floor(Math.random()*100+1);
+}
+function exibeTextoTag(tag,texto){  
+    let varTag = document.querySelector(tag);
+    varTag.innerHTML = texto;
+    responsiveVoice.speak(texto, 'UK English Male');    
+}
+function inicializaTexto(){
+    exibeTextoTag('h1','Secret Number');
+    exibeTextoTag('p','Type a number from 1 to 10, you have 10 attempts');
+}
 function verificarChute(){
     let guess = document.querySelector('input').value;
     if(guess == secret_number){
         let word_attempts = trys > 1 ? 'attempts': 'attempt';
-        varH1.innerHTML = 'Congratulations';
-        varP.innerHTML = `You won with ${trys} ${word_attempts}`;
-        document.getElementById('reiniciar').removeAttribute('disabled');
-        document.getElementById("chutar").disabled = true; //botão desativado, para começar outro jogo precisar clicar no botão novo jogo
+        exibeTextoTag('h1','Congratulations');
+        exibeTextoTag('p', `You won with ${trys} ${word_attempts}`);
+        desabilitarImput();
+        resetBotoes(); //botão desativado, para começar outro jogo precisar clicar no botão novo jogo    
     }
     else{
         if(guess < secret_number){
-            varP.innerHTML = 'Wrong, try again the number is higher';
+            exibeTextoTag('p','Wrong, try again the number is higherㅤㅤㅤㅤㅤㅤ');
         }
         else{
-            varP.innerHTML = 'Wrong, try again the number is lower';
+            exibeTextoTag('p','Wrong, try again the number is lowerㅤㅤㅤㅤㅤㅤ');
         }
         trys++;
-    }       
+    }
+    if(trys == 10){
+        exibeTextoTag('p', 'YOU LOST');
+        desabilitarImput();
+        resetBotoes();
+    }
+    limpaImput();
+}
+function desabilitarImput(){
+    document.querySelector('input').disabled = true;
+}
+function habilitarImput(){
+    document.querySelector('input').removeAttribute('disabled');
+}
+function resetBotoes(){
+    document.getElementById('reiniciar').removeAttribute('disabled');
+    document.getElementById("chutar").disabled = true;
+}
+function limpaImput(){
+    document.querySelector('input').value = '';      
 }
 function novojogo(){ //iniciar um novo jogo com tudo resetado
     trys = 1;
-    secret_number = Math.floor(Math.random() * 10 + 1);
-    varH1.innerHTML = 'Secret Number';
-    varP.innerHTML = 'Type a number from 1 to 10';
+    secret_number = getSecretNumber();
+    inicializaTexto();
     document.getElementById('chutar').removeAttribute('disabled');
     document.getElementById('reiniciar').disabled = true;
-    document.querySelector('input').value = '';
+    habilitarImput();
+    limpaImput();
 }
+inicializaTexto(); //inicializa o texto quando entra ou atualiza a pagina
